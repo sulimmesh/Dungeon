aa5d589a2741f696b31b9857b10d58a07f9444ca
Steve <steven.helferich@gmail.com>
2015-03-20 22:06:55 -0700

    adding html data elements and item collision

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-var UP = "87", DOWN = "83", LEFT = "65", RIGHT = "68", X = 5, Y = 5, XSIZE = 10, YSIZE = 5;
+var UP = "87", DOWN = "83", LEFT = "65", RIGHT = "68", X = 10, Y = 5, XSIZE = 10, YSIZE = 5;
 var c = document.getElementById("canvas");
 var ctx = c.getContext("2d");
 var xPos, yPos;
+var itemPos = [100,50];
 
 window.addEventListener("load",init);
 document.addEventListener('keydown',move);
@@ -18,11 +19,24 @@ function clear() {
 }
 
 function reDraw(delX, delY) {
-	clear();
-	ctx.fillStyle = "black";
-	xPos = xPos+delX;
-	yPos = yPos+delY;
-	ctx.fillRect(xPos,yPos,XSIZE,YSIZE);
+	if (xPos+delX > 0 && xPos+delX < 300 && 
+		yPos+delY > 0 && yPos+delY < 150) {
+		clear();
+		ctx.fillStyle = "black";
+		xPos = xPos+delX;
+		yPos = yPos+delY;
+		ctx.fillRect(xPos,yPos,XSIZE,YSIZE);
+		if (itemPos[0] >= xPos-X && itemPos[0] <= xPos+X 
+			&& itemPos[1] >- yPos-Y && itemPos[1] <= yPos+Y) {
+			var data = document.getElementById("data-item");
+			data.setAttribute("data-item","item stuff");
+		}
+		//making random item to pick up
+		var itemX = 100;
+		var itemY = 50;
+		ctx.fillRect(itemX,itemY,XSIZE,YSIZE);
+
+	}
 }
 
 function move(ev) {
